cd1b013134c6e5debfaa76984995a60ab9099f46
Apti Kapo <joker7789@gmail.com>
2013-03-17 17:07:33 +0600

    update escape example

--- php-basics/basic_escape.php
+++ php-basics/basic_escape.php
@@ -8,3 +8,8 @@ echo "\r\n"; // out: new line
 echo '\r\n'; // out: \r\n
 
 echo '$a = $a'; // out: $a = $a
+
+$stuff = array(1,2,3);
+
+echo "\r\n000$stuff[1]000\r\n"; // out: 0002000
+echo "000{$stuff[1]}000"; // out: 0002000

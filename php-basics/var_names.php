518a3c6228b7ba605e208abf1f34f882aa1e11c8
Apti Kapo <joker7789@gmail.com>
2013-03-17 17:01:16 +0600

    update var_names with comments

--- php-basics/var_names.php
+++ php-basics/var_names.php
@@ -2,10 +2,12 @@
 
 $_var = 5;
 $_stuff_6 = 3;
-$varName = 20;
-$varname = 21;
+$varName = 20; // these two 
+$varname = 21; // differ 
 
 $_10_var = 10;
 
 echo $varname."\r\n";
 echo $varName."\r\n";
+
+// $50_var - invalid var name

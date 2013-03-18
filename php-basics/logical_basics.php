8e1232fafd8f7b00d258992802f7f974e23024c5
Apti Kapo <joker7789@gmail.com>
2013-03-19 02:13:58 +0600

    logical string to bool

---
+++ php-basics/logical_basics.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump(true==false); // false
+
+echo "\r\n";
+
+var_dump((bool)"heey"); // true
+
+echo "\r\n";
+
+var_dump((bool)""); // false
+
+echo "\r\n";
+
+var_dump((bool)0); // false
+
+echo "\r\n";
+
+var_dump((bool)'0'); // false
---
+++ php-basics/string_stuff.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "10"==10; // 1
+
+echo "\r\n";
+
+echo 25 == "25boo"; // 1
+
+echo "\r\n";
+
+echo 1+'aaa'+1; // 2 : "aaa" is being to integer first, which is 0
+
+echo "\r\n";
+
+echo "xx" + "yy"; // 0 : same as with previous example
+
+echo "\r\n";
+
+echo (integer)"777stuff"; // 777
+
+echo "\r\n";
+
+var_dump("10"===10); // false
+
+echo "\r\n";
+
+echo (integer)"stuff888"; // 0
